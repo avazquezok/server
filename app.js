16cb7d78ca3c5aaeb3f8184da0f3922d77b5a697
dullkarlmarx <agustinvazquezrocha33@gmail.com>
2022-11-20 19:33:18 -0300

    1.0

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
 const express = require('express');
+const { readFile } = require('fs').promises;
 const app = express();
-app.get()
+
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    next();
+});
+app.get('/', async (request, response) => {
+    console.log(request.url);
+    let file = request.url.substring(2)  // le saca del principio '/?' a la url porque llega como '/?emercado-api...'
+    file = './' + file
+    response.send(await readFile(file, 'utf8'))
+});
+
+
 app.listen(3000);
